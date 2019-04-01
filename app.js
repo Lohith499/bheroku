b531c5fc20f177b1ad7322e79ca4414f53c86658
Lohith499 <36016755+Lohith499@users.noreply.github.com>
2019-04-01 01:31:11 -0400

    logging logged out users

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ require('dotenv').config();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.set('trust proxy', true);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
